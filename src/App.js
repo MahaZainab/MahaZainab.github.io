f1563469465117cf195a0618af5ed67a1314f888
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-07-23 06:25:09 +0500

    Bootstrap css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
