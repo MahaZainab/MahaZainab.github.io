e309a62823853a3a4a5e3e013df959d3c8a04fe6
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2026-05-10 13:28:06 -0500

    Auto commit on save

--- src/components/books.tsx
+++ src/components/books.tsx
@@ -42,7 +42,7 @@ export default function BooksSection() {
     <section id="books" className="py-10 bg-white">
       <div className="max-w-6xl mx-auto px-6 xl:px-0">
         <div className="text-left lg:text-center mb-10">
-          <h2 className="text-5xl font-bold mb-4 bg-linear-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent">
+          <h2 className="text-3xl font-bold mb-4 bg-linear-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent">
             Books
           </h2>
         </div>
@@ -52,9 +52,9 @@ export default function BooksSection() {
             <div key={year} className="space-y-2">
               <div className="flex items-center gap-3">
                 <Calendar className="w-6 h-6 text-blue-600" />
-                <h3 className="text-3xl font-bold text-gray-900">{year}</h3>
+                <h3 className="text-xl font-bold text-gray-900">{year}</h3>
                 <div className="flex-1 h-px bg-gray-300 ml-4"></div>
-                <span className="text-sm text-gray-500 bg-gray-100 px-3 py-1 rounded-full">
+                <span className="text-xs text-gray-500 bg-gray-100 px-3 py-1 rounded-full">
                   {visibleGroups[year]?.length} book
                   {visibleGroups[year]?.length > 1 ? "s" : ""}
                 </span>
